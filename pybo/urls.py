f8321f0cb7f09437ae9ab6fc0571361f8bab1589
alias-son <43431381+alias-son@users.noreply.github.com>
2021-05-04 14:40:12 +0900

    update

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views   # 3-11에서 지워짐
-from .views import base_views, question_views, answer_views, comment_views
+from .views import base_views, question_views, answer_views, comment_views, vote_views
 
 app_name = 'pybo'
 
@@ -41,6 +41,9 @@ urlpatterns = [
          comment_views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/',
          comment_views.comment_delete_answer, name='comment_delete_answer'),
+    # vote_views.py
+    path('vote_views.py/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
+    path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
 
     # 3-11에서 지워짐짐
    # path('', views.index, name='index'),
